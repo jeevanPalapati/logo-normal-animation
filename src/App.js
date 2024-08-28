1ba7f84afcbca82434ebeb0b9b6016c6ba0f96d5
jeevanPalapati <jeevankumarpalapati@ve.ai>
2024-08-28 15:41:20 +0530

    Adding files to git remote

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import LogoAnimation from './components/LogoAnimation';
+import "./App.css"
+function App() {
+  return (
+    <div className='App'>
+      <LogoAnimation />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/LogoAnimation.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import {ReactComponent as Logo} from "../assets/Logo.svg"
+import "./LogoAnimation.css"
+
+const LogoAnimation = () => {
+  return (
+    <div className='mainBody' >
+        <div className='mainDiv' >
+          <div className='logoParent'>
+            
+        <Logo className='logoDiv'/>
+          </div>
+        <div className='text'>Own Your Future</div>
+   {/*    <Logo className='logoDiv1'/>
+      <Logo className='logoDiv2'/>
+      <Logo className='logoDiv3'/>
+      <Logo className='logoDiv4'/>
+      <Logo className='logoDiv5'/>
+      <Logo className='logoDiv6'/>
+      <Logo className='logoDiv7'/>
+      <Logo className='logoDiv8'/>
+      <Logo className='logoDiv9'/>
+      <Logo className='logoDiv10'/> */}
+      </div>
+    </div>
+  )
+}
+
+export default LogoAnimation
